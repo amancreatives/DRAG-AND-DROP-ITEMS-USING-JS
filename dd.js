a008ad9a5f239c3b06d5b416ba5305fa573f4993
AMAN KUKREJA <kukrejaaman2006@gmail.com>
2024-07-21 20:10:48 +0530

    Add files via upload

---
+++ dd.js
@@ -0,0 +1,27 @@
+let lists= document.getElementsByClassName("list");
+let rightBox = document.querySelector("#right");
+let leftBox = document.querySelector("#left");
+
+
+for(i of lists){
+    i.addEventListener("dragstart", function(e){
+        let selected = e.target;
+
+        rightBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+        rightBox.addEventListener("drop", function(e){
+            rightBox.appendChild(selected);
+            selected = null
+        });
+
+        leftBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+        leftBox.addEventListener("drop", function(e){
+            leftBox.appendChild(selected);
+            selected = null
+        });
+    })
+
+}
